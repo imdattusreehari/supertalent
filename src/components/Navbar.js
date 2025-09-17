269408e98d5067e28aa5b6f38085fce708d971da
imdattusreehari <dattusreeharimudragada@gmail.com>
2025-09-17 19:27:31 +0530

    added portfolio page to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
 
         <nav className="nav-links">
           <a href="/">Home</a>
-          <a href="/pages/portfolio">Portfolio</a>
+          <a href="/portfolio">Portfolio</a>
           <a href="/pages/portfolio">Resources</a>
           <a href="/pages/portfolio">About Us</a>
           <a href="/contact-us">Contact Us</a>
